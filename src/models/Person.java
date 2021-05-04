2a39164eaa274aeec8e326c38aee1cd8e2aaa7b6
andresmauricio <andresacelas@outlook.com>
2021-05-04 09:15:54 -0600

    model person

---
+++ src/models/Person.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author PC
+ */
+public class Person {
+    
+    	public int id;
+	public String nombres;
+	public String identificacion;
+	public String nacimiento;
+	public String telefono;
+        public String direccion;
+	public String password;
+	public String rol;
+    
+}
